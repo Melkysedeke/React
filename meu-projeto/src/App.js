36bc2ac0bf55fb54635a2721540c106c1799782c
Melkysedeke <melkybahia88@gmail.com>
2024-05-18 13:46:39 -0300

    Adicionando CSS

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   const nome = "Maria"
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <Frase/>
       <SayMyName nome="Melky"/>
       <SayMyName nome="José"/>
       <SayMyName nome={nome}/>
-      <Pessoa nome="Melky" idade="21" profissao="Programador" foto="https://via.palceholder.com/150"/>
+      <Pessoa nome="Melky" idade="21" profissao="Programador" foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
