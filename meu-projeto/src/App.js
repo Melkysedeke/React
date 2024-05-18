eb95acd85233dbc7bccfebf46c8c328cec8586b3
Melkysedeke <melkybahia88@gmail.com>
2024-05-18 13:29:31 -0300

    Props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = "Melky"
-
-  const newName = name.toUpperCase()
-
-  function sum(a,b){
-    return a+b
-  }
-
-  const url = "https://via.placeholder.com/150"
-  
+  const nome = "Maria"
   return (
     <div className="App">
-      <h1>Alterando JSX</h1>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1,2)}</p>
-      <img src={url} alt="Minha imagem"></img>
-      <HelloWorld/>
+      <SayMyName nome="Melky"/>
+      <SayMyName nome="José"/>
+      <SayMyName nome={nome}/>
+      <Pessoa nome="Melky" idade="21" profissao="Programador" foto="https://via.palceholder.com/150"/>
     </div>
   );
 }
