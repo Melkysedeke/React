3f9b1cbb25c3db86f9878bd23c422ee5c18e0ab8
Melkysedeke <melkybahia88@gmail.com>
2024-05-22 21:14:28 -0300

    React icons

--- meu-projeto/src/pages/Home.js
+++ meu-projeto/src/pages/Home.js
@@ -1,10 +1,10 @@
-function Home(){
-    return(
-        <div>
-            <h1>Home</h1>
-            <p>Conteúdo da página</p>
-        </div>
-    )
-}
+    function Home(){
+        return(
+            <div>
+                <h1>Home</h1>
+                <p>Conteúdo da página</p>
+            </div>
+        )
+    }
 
-export default Home
+    export default Home
