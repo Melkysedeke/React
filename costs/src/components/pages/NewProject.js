fb2c745c74953ac956f3c068b37a4df85d4b01b0
Melkysedeke <melkybahia88@gmail.com>
2024-05-25 17:33:26 -0300

    Inserindo dados no banco via API

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,12 +1,35 @@
 import ProjetctForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
+import {useNavigate} from 'react-router-dom'
 
 function NewProject(){
+    const navigate = useNavigate()
+
+    function createPost(project){
+        //initialize cost and services
+        project.cost=0
+        project.services=[]
+        fetch('http://localhost:5000/projects', {
+            method: 'POST',
+            heraders: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp)=>resp.json())
+        .then((data)=>{
+            console.log(data)
+            //redirect
+            navigate('/projects', {message: 'Projeto criado com sucesso!'})
+        })
+        .catch((err)=>console.log(err))
+    }
+
     return(
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjetctForm btnText="Criar Projeto"/>
+            <ProjetctForm handleSubmit={createPost} btnText="Criar Projeto"/>
         </div>
     )
 }
