d12bc52b48fac056b36ecdafc8ab07659ee68bcf
Melkysedeke <melkybahia88@gmail.com>
2024-05-26 22:42:06 -0300

    Criando componente loader

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -5,9 +5,11 @@ import styles from "./Projects.module.css"
 import Container from "../layout/Container"
 import LinkButton from "../layout/LinkButton"
 import ProjectCard from "../project/ProjectCard"
+import Loading from "../layout/Loading"
 
 function Projects(){
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false )
 
     const location = useLocation()
     let message = ''
@@ -17,17 +19,20 @@ function Projects(){
     }
 
     useEffect(()=>{
-        fetch('http://localhost:5000/projects', {
-            method:'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        .then(resp=>resp.json())
-        .then(data=>{
-            setProjects(data)
-        })
-        .catch((err)=>console.log(err))
+        setTimeout(()=>{
+            fetch('http://localhost:5000/projects', {
+                method:'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+            .then(resp=>resp.json())
+            .then(data=>{
+                setProjects(data)
+                setRemoveLoading(true)
+            })
+            .catch((err)=>console.log(err))
+        }, 500)
     },[])
 
     return(
@@ -41,6 +46,10 @@ function Projects(){
                 {projects.length > 0 && projects.map((project)=>(
                     <ProjectCard id={project.id} name={project.name} budget={project.budget} category={project.category.name} key={project.id}/>
                 ))}
+                {!removeLoading && <Loading/>}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
